ea7fcae1e3095a89cd0db7df0a64f313f5911d75
Marc Page <MarcAllenPage@gmail.com>
2024-04-03 06:17:11 -0500

    updates

--- devopsdriver/azure/workitem/wiql.py
+++ devopsdriver/azure/workitem/wiql.py
@@ -22,7 +22,7 @@ ASOF '02-11-2020'
 from datetime import datetime, date
 
 
-class Field:
+class Field:  # pylint disable=too-few-public-methods
     """column name"""
 
     SYSTEM = {
@@ -43,7 +43,7 @@ class Field:
         return f"[{'System.' if self.value in Field.SYSTEM else ''}{self.value}]"
 
 
-class OrderBy:
+class OrderBy:  # pylint disable=too-few-public-methods
     """Order by field"""
 
     def __init__(self, field: Field, order: str = "DESC"):
@@ -54,7 +54,7 @@ class OrderBy:
         return f"{self.field} {self.order}"
 
 
-class Ascending:
+class Ascending:  # pylint disable=too-few-public-methods
     """Order by field ascending"""
 
     def __init__(self, field: Field):
@@ -62,12 +62,35 @@ class Ascending:
         super().__init__(field, "ASC")
 
 
-class Descending:
+class Descending:  # pylint disable=too-few-public-methods
     """Order by field descending"""
 
     def __init__(self, field: Field):
         self.field = field
         super().__init__(field, "DESC")
+
+
+class Value:  # pylint disable=too-few-public-methods
+    """A constant value"""
+
+    def __init__(self, value: any):
+        self.value = value
+
+    def __str__(self) -> str:
+        if isinstance(self.value, datetime):
+            return self.value.strftime("%Y-%m-%d %H:%M:%S")
+
+        if isinstance(self.value, date):
+            return self.value.strftime("%Y-%m-%d")
+
+        if isinstance(self.value, int):
+            return str(self.value)
+
+        if isinstance(self.value, float):
+            return f"{self.value:0.3f}"
+
+        if isinstance(self.value, str):
+            return f'"{self.value}"'  # TODO: escape value  # pylint: disable=fixme
 
 
 class Builder:
@@ -97,15 +120,21 @@ class Builder:
         self.selected = fields
         return self
 
-    @staticmethod
-    def __format(entity) -> str:
-        assert isinstance(entity, datetime) or isinstance(entity, date)
-        fmt = "%Y-%m-%d" if isinstance(entity, date) else "%Y-%m-%d %H:%M:%S"
-        return f"'{entity.strftime(fmt)}'"
+    def asof(self, date: Value):
+        """Set the view of the data
+
+        Args:
+            date (Value): The date or datetime in a Value
+
+        Returns:
+            Builder: self for chainingd
+        """  # TODO: allow date datetime or Value  # pylint: disable=fixme
+        self.snapshot = date
+        return self
 
     def __str__(self) -> str:
         select = ", ".join(str(s) for s in self.selected)
         where = f" WHERE {self.search}" if self.search else ""
         order = f" ORDER BY {', '.join(self.order)}" if self.order else ""
-        asof = f" ASOF {Builder.__format(self.snapshot)}" if self.snapshot else ""
+        asof = f" ASOF {str(self.snapshot)}" if self.snapshot else ""
         return f"SELECT {select} FROM workitems{where}{order}{asof}"
