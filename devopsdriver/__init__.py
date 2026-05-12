f7c753f684c6bbaf9b09b63f72538731a541709b
Marc Page <MarcAllenPage@gmail.com>
2026-05-12 14:02:54 -0500

    Release 0.1.54

--- devopsdriver/__init__.py
+++ devopsdriver/__init__.py
@@ -6,6 +6,6 @@ from .template import Template
 from .azdo.clients import Azure
 from .github.client import Github
 
-__version__ = "0.1.53"
+__version__ = "0.1.54"
 __author__ = "Marc Page"
 __credits__ = ""
