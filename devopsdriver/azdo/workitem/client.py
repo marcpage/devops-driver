489eec0ef3d3c2160c9b34e521bc6ed13467bf5f
Marc Page <MarcAllenPage@gmail.com>
2026-08-31 12:57:04 -0500

    Add fetch_work_items to azdo client

--- devopsdriver/azdo/workitem/client.py
+++ devopsdriver/azdo/workitem/client.py
@@ -3,12 +3,20 @@
 
 """Azure WorkItem Client"""
 
+from typing import TypeVar
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+
 from azure.devops.v7_1.work_item_tracking.models import Wiql as AzureWiql
 from azure.devops.v7_1.work_item_tracking.models import WorkItem as AzureWorkItem
 from azure.devops.v7_1.work_item_tracking.models import TeamContext
 from azure.devops.v7_1.work_item_tracking.models import WorkItemQueryResult
+from azure.devops.exceptions import AzureDevOpsServiceError
+
 from devopsdriver.azdo.azureobject import AzureObject
 from devopsdriver.azdo.workitem.wiql import Wiql
+
+T = TypeVar("T")
 
 
 class Client:
@@ -41,6 +49,63 @@ class Client:
             time_precision=time_precision,
             top=top,
         )
+
+    def _fetch_work_items_batch(
+        self, ids: list[int], cls: type[T], expand: str | None = None
+    ) -> list[T]:
+        """Fetch a batch of work items, skipping deleted items."""
+
+        if not ids:
+            return []
+
+        try:
+            info_list = self.client.get_work_items(
+                ids=ids,
+                expand=expand,
+            )
+
+            return [AzureObject(info).convert_to(cls) for info in info_list]
+
+        except AzureDevOpsServiceError:
+            if len(ids) == 1:
+                return []
+
+        midpoint = len(ids) // 2
+        first_half = self._fetch_work_items_batch(ids[:midpoint], cls, expand)
+        return [
+            *first_half,
+            *self._fetch_work_items_batch(ids[midpoint:], cls, expand),
+        ]
+
+    def fetch_work_items(  # pylint: disable=too-many-arguments,too-many-positional-arguments
+        self,
+        ids: list[int],
+        cls: type[T],
+        expand: str | None = None,
+        batch_size: int = 200,
+        max_threads: int = 8,
+    ) -> list[T]:
+        """Fetch work items in parallel batches, skipping deleted items."""
+        if not ids:
+            return []
+
+        if batch_size <= 0:
+            raise ValueError("batch_size must be greater than zero")
+
+        if max_threads <= 0:
+            raise ValueError("max_threads must be greater than zero")
+
+        batches = [ids[i : i + batch_size] for i in range(0, len(ids), batch_size)]
+        fetch_batch = partial(
+            self._fetch_work_items_batch,
+            cls=cls,
+            expand=expand,
+        )
+
+        with ThreadPoolExecutor(max_workers=max_threads) as executor:
+            results = executor.map(fetch_batch, batches)
+
+        return [item for batch in results for item in batch]
 
     def get_history(  # pylint: disable=too-many-positional-arguments,too-many-arguments
         self,
